e46f843362b9b87b4ad028d91bb33237313c3e7b
igor <igor@parallels.com>
2010-09-17 10:16:42 +0000

    MAJOR @32184
    Review @32184 "fixed linking with non NPTL pthread"
    Reviewed-by: dim
    
    fixed linking with non NPTL pthread

--- src/vzevent_error.c
+++ src/vzevent_error.c
@@ -17,12 +17,39 @@ struct last_error_info {
         char msg[1024];
 };
 
+#ifdef __i386__
+#include <pthread.h>
+/* Workaround for non NPTL glibc
+ * Private thread specific data */
+static pthread_key_t last_err_local_storage_key;
+static pthread_once_t last_err_local_storage_key_once = PTHREAD_ONCE_INIT;
+
+static void buffer_destroy(void *buf)
+{
+	free(buf);
+}
+
+static void buffer_key_alloc(void)
+{
+	pthread_key_create(&last_err_local_storage_key, buffer_destroy);
+	pthread_setspecific(last_err_local_storage_key,
+			calloc(1, sizeof(struct last_error_info)));
+}
+
+static struct last_error_info *get_last_error_local_storage()
+{
+	pthread_once(&last_err_local_storage_key_once, buffer_key_alloc);
+	return pthread_getspecific(last_err_local_storage_key);
+}
+#else
+
 static __thread struct last_error_info last_err_local_storage;
 
 static struct last_error_info *get_last_error_local_storage()
 {
 	return &last_err_local_storage;
 }
+#endif
 
 static struct last_error_info *get_last_error_storage()
 {
